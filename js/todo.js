c158a1c26d51d8bf5ba07710ff17c64251e993d2
Mason3144 <lts890303@gmail.com>
2022-01-04 23:11:59 -0800

    Todo List

--- js/todo.js
+++ js/todo.js
@@ -2,27 +2,40 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
-const toDos = [];
+const TODOS_KEY = "todos"
+
+let toDos = [];
 
 function saveToDos() {
-    localStorage.setItem("todos", JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event) {
     // toDoList.removeChild(event.target.parentElement);
     const li = event.target.parentElement;
     li.remove();
+
+
+    // function filters(toDo) {
+    //     const listId = parseInt(li.id);
+    //     return toDo.id !== listId;
+    // }
+    // const sample = toDos.filter(filters)
+    // toDos = sample;
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
     const btn = document.createElement("button");
     li.appendChild(span);
     li.appendChild(btn);
     toDoList.appendChild(li);
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     btn.innerText = "x";
     btn.addEventListener("click", deleteToDo);
 
@@ -35,8 +48,12 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id:Date.now()
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
@@ -44,4 +61,13 @@ toDoForm.addEventListener("submit", handleToDoSubmit);
 
 
 
+
+const savedToDos = localStorage.getItem(TODOS_KEY)
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);       
+}
+    
 
